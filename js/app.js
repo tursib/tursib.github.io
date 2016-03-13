7b53d9b65751b28836350d831519fe823e58b28e
Ilincescu Mihai <mihai@ilincescu.ro>
2016-03-13 10:53:30 +0200

    Added the facebook and google login

--- js/app.js
+++ js/app.js
@@ -79,6 +79,50 @@ Vidpub.loginGithub = function(next){
   });
 };
 
+Vidpub.loginFacebook= function(next){
+  Vidpub.dbRef.authWithOAuthPopup("facebook", function(error, authData) {
+    if (error) {
+      if (error.code === "TRANSPORT_UNAVAILABLE") {
+        // fall-back to browser redirects, and pick up the session
+        // automatically when we come back to the origin page
+        ref.authWithOAuthRedirect("github", function(error) {
+          next(err,null);
+        });
+      }
+    } else if (authData) {
+      var userData = {
+        name: authData.github.displayName,
+        email: authData.github.email,
+        githubber: true
+      }
+      Vidpub.saveUser(authData.uid, userData, next);
+
+    }
+  });
+};
+
+Vidpub.loginGoogle= function(next){
+  Vidpub.dbRef.authWithOAuthPopup("google", function(error, authData) {
+    if (error) {
+      if (error.code === "TRANSPORT_UNAVAILABLE") {
+        // fall-back to browser redirects, and pick up the session
+        // automatically when we come back to the origin page
+        ref.authWithOAuthRedirect("github", function(error) {
+          next(err,null);
+        });
+      }
+    } else if (authData) {
+      var userData = {
+        name: authData.github.displayName,
+        email: authData.github.email,
+        githubber: true
+      }
+      Vidpub.saveUser(authData.uid, userData, next);
+
+    }
+  });
+};
+
 var loadCurrentUser = function(next){
   var authData = Vidpub.dbRef.getAuth();
   if(authData){
